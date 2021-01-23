75d8002c00645bc347691775d085bd5ffe3a2807
User <User@DESKTOP-ENT97T7>
2021-01-23 06:58:20 +0200

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "fatih", "mehmet", "hilal", "bilal" };
+            //console.writeline(isimler[0]);
+            //console.writeline(isimler[1]);
+            //console.writeline(isimler[2]);
+            //console.writeline(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Fatih", "Mehmet", "Hilal", "bilal" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            isimler2.Add("Büşra");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
